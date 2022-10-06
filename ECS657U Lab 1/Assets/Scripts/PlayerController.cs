1e32142ecc7035a1aa519ac92180f656fe335be8
cegroen <ce12groen@gmail.com>
2022-10-06 10:51:19 +0100

    Added text to game

--- ECS657U Lab 1/Assets/Scripts/PlayerController.cs
+++ ECS657U Lab 1/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
     
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     void Start() {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void OnMove(InputValue value) {
@@ -27,6 +34,14 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.tag == "PickUp") {
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText() {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups) {
+            winText.text = "You win!";
         }
     }
 }
